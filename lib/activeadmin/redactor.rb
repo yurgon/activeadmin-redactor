324685190173b3380c8da0e7ea83521b59e174ab
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-08-26 13:32:00 +0100

    Added basic editor
    * Renders empty textfield with no content
    * Added javascript / stylesheet assets

--- lib/activeadmin/redactor.rb
+++ lib/activeadmin/redactor.rb
@@ -1,8 +1,18 @@
 require 'activeadmin/redactor/version'
 require 'activeadmin'
+require 'rails/engine'
 
 module Activeadmin
   module Redactor
+    module ControllerActions
+      def redactorable
+        member_action :redactor do
+
+          render 'redactor/editor', layout: false
+        end
+      end
+    end
+
     module TableMethods
       def redactorable_edit_icon
         column 'Redactor', class: 'activeadmin-redactorable' do |resource|
@@ -13,6 +23,10 @@ module Activeadmin
       end
     end
 
+    ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
     ::ActiveAdmin::Views::TableFor.send(:include, TableMethods)
+
+    class Engine < ::Rails::Engine
+    end
   end
 end
